e0f958e353c55b3794a56ae0b0163c9e45f53698
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-11-08 19:08:13 -0500

    feat: support ip6 mirrors

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { publicIpv4 } = require("./ip-utils");
 
 const run = async () => {
-  const ip = await publicIpv4();
+  const ip = await publicIpv4({ forceIp4: true });
   console.log(ip);
 };
 
